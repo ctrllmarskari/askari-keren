bc5ec06820613294d28a85bdba9681b7557e1af2
skare <candraayulisa@gmail.com>
2026-02-18 14:26:09 +0700

    Create Javascript.css.js

---
+++ Javascript.css.js
@@ -0,0 +1,8 @@
+var text = new createjs.Text(
+"love u Nikita Dila Ayu Kirana\nthe longer i'm w\nthe more i love u
+"bold 24px Arial", "#fff");
+teext.textAlign = "center";
+text.x = w / 2;
+text.y  h / 2 - text.
+  getMeasuredLineHeight();
+stage.addChild(text);
